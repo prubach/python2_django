11e1003c4be3ecf1336392207e70b511d286721f
prubac <pawel.rubach@sgh.waw.pl>
2020-01-09 15:13:24 +0100

    Present data in a page

--- bank/bankapp/urls.py
+++ bank/bankapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('t', views.test, name='test')
+    path('t', views.test, name='test'),
+    path('<int:cust_id>/', views.cust, name='customer')
 ]
--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from .models import Customer
+
 
 def index(request):
     return HttpResponse('Hello from Bank App')
@@ -10,3 +13,8 @@ def test(request):
     body = 'Hello from a template'
     title = 'My first template'
     return render(request, 'bankapp/test.html', {'body_text': body, 'title': title})
+
+
+def cust(request, cust_id):
+    customer = get_object_or_404(Customer, pk=cust_id)
+    return render(request, 'bankapp/cust.html', {'customer': customer})
